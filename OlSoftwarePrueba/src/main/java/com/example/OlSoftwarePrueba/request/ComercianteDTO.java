3d8589d6516d77106b6fa3f5c9c92a7b00373980
deriver1989 <46784816+deriver1989@users.noreply.github.com>
2025-05-25 17:35:29 -0500

    ARCHIVOS SQL Y COLECCION POSTMAN

--- OlSoftwarePrueba/src/main/java/com/example/OlSoftwarePrueba/request/ComercianteDTO.java
+++ OlSoftwarePrueba/src/main/java/com/example/OlSoftwarePrueba/request/ComercianteDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ComercianteDTO {
 
     @Email
